c414743c1436a7b0cb46acac07a41ffb937036aa
El Akpadetsi <akpadetsi@gmail.com>
2023-11-23 00:50:37 +0000

    Modified EC2 Instance

--- first_ec2.tf
+++ first_ec2.tf
@@ -1,13 +1,13 @@
 provider "aws" {
-    region = "us-east-1"
-    profile = "DevOps-Admin"
+  region  = "us-east-1"
+  profile = "DevOps-Admin"
 }
 
 resource "aws_instance" "First_Instance" {
-    ami = "ami-0230bd60aa48260c6"
-    instance_type = "t2.micro"
-  
-    tags = {
-       Name = "My Web Server"
-         }
+  ami           = "ami-0230bd60aa48260c6"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "My Web Server"
+  }
 }
